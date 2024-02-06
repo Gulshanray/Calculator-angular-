0fd527ac63ff05db47b03ff516ddae85d1614d4f
Gulshanray <raygulshan960@gmail.com>
2024-02-06 10:44:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,88 @@
+// app.component.ts
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div class="calculator">
+      <div class="calculator__output">{{ displayValue }}</div>
+      <div class="calculator__keys">
+        <button class="calculator__key calculator__key--operator" (click)="onOperatorClick('+')">+</button>
+        <button class="calculator__key calculator__key--operator" (click)="onOperatorClick('-')">-</button>
+        <button class="calculator__key calculator__key--operator" (click)="onOperatorClick('*')">&times;</button>
+        <button class="calculator__key calculator__key--operator" (click)="onOperatorClick('/')">&divide;</button>
+        <button class="calculator__key" (click)="onNumberClick(7)">7</button>
+        <button class="calculator__key" (click)="onNumberClick(8)">8</button>
+        <button class="calculator__key" (click)="onNumberClick(9)">9</button>
+        <button class="calculator__key" (click)="onNumberClick(4)">4</button>
+        <button class="calculator__key" (click)="onNumberClick(5)">5</button>
+        <button class="calculator__key" (click)="onNumberClick(6)">6</button>
+        <button class="calculator__key" (click)="onNumberClick(1)">1</button>
+        <button class="calculator__key" (click)="onNumberClick(2)">2</button>
+        <button class="calculator__key" (click)="onNumberClick(3)">3</button>
+        <button class="calculator__key" (click)="onNumberClick(0)">0</button>
+        <button class="calculator__key" (click)="onDecimalClick()">.</button>
+        <button class="calculator__key" (click)="onClearClick()">AC</button>
+        <button class="calculator__key calculator__key--enter" (click)="onEqualClick()">=</button>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dehradun';
+  displayValue: string = '0';
+  operator: string | null = null;
+  firstOperand: number | null = null;
+
+  onNumberClick(number: number): void {
+    if (this.displayValue === '0') {
+      this.displayValue = number.toString();
+    } else {
+      this.displayValue += number;
+    }
+  }
+
+  onOperatorClick(operator: string): void {
+    if (this.operator === null) {
+      this.firstOperand = +this.displayValue;
+      this.operator = operator;
+      this.displayValue = '0';
+    }
+  }
+
+  onEqualClick(): void {
+    if (this.operator && this.firstOperand !== null) {
+      const secondOperand = +this.displayValue;
+      switch (this.operator) {
+        case '+':
+          this.displayValue = (this.firstOperand + secondOperand).toString();
+          break;
+        case '-':
+          this.displayValue = (this.firstOperand - secondOperand).toString();
+          break;
+        case '*':
+          this.displayValue = (this.firstOperand * secondOperand).toString();
+          break;
+        case '/':
+          this.displayValue = (this.firstOperand / secondOperand).toString();
+          break;
+      }
+      this.operator = null;
+      this.firstOperand = null;
+    }
+  }
+
+  onClearClick(): void {
+    this.displayValue = '0';
+    this.operator = null;
+    this.firstOperand = null;
+  }
+
+  onDecimalClick(): void {
+    if (!this.displayValue.includes('.')) {
+      this.displayValue += '.';
+    }
+  }
 }
